04a0bafd45472d4e937b2a5829f7b779725df351
Ondřej Synáček <ondrej@synacek.org>
2023-10-19 22:37:48 +0200

    refactor: simplify function call to check panel buttons

--- js/ui/panel.mjs
+++ js/ui/panel.mjs
@@ -2,15 +2,11 @@ import { TOOL_LIST, COLOR_LIST } from "../state/state.mjs";
 import { setTool, setColor } from "../state/actions.mjs";
 import { getPanel, getPanelTools, getPanelColors } from "../dom.mjs";
 
-function isCursorWithinPanelBounds(x, y) {
-  const panel = getPanel();
-  const rect = panel.getBoundingClientRect();
-
+function isCursorWithinPanelBounds(x, y, rect) {
   return x >= rect.left && x <= rect.right && y >= rect.top && y <= rect.bottom;
 }
 
-function getPanelButtonByCoordinates(x, y) {
-  const panel = getPanel();
+function getPanelButtonByCoordinates(x, y, panel) {
   const buttons = panel.querySelectorAll("button");
 
   for (let i = 0; i < buttons.length; i++) {
@@ -29,7 +25,14 @@ function getPanelButtonByCoordinates(x, y) {
 }
 
 function activatePanelButtonOnCoordinates(x, y) {
-  const button = getPanelButtonByCoordinates(x, y);
+  const panel = getPanel();
+  const rect = panel.getBoundingClientRect();
+
+  if (!isCursorWithinPanelBounds(x, y, rect)) {
+    return;
+  }
+
+  const button = getPanelButtonByCoordinates(x, y, panel);
 
   if (!button) {
     return;
@@ -40,10 +43,6 @@ function activatePanelButtonOnCoordinates(x, y) {
 
 export function attachPanelListeners({ state }) {
   window.addEventListener("click", (event) => {
-    if (!isCursorWithinPanelBounds(event.clientX, event.clientY)) {
-      return;
-    }
-
     activatePanelButtonOnCoordinates(event.clientX, event.clientY);
   });
 

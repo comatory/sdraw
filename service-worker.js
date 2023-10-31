e031227d71a232e83792447197a3e817fe5553f9
Ondřej Synáček <ondrej@synacek.org>
2023-10-31 22:00:42 +0100

    improvement: do not use ES module with service worker
    This was giving me some headaches on my web server.

--- service-worker.js
+++ service-worker.js
@@ -1,5 +1,5 @@
 const STATIC_CACHE_NAME = "static";
-const STATIC_CACHE_VERSION = "v21";
+const STATIC_CACHE_VERSION = "v22";
 const STATIC_CACHE_ID = `${STATIC_CACHE_NAME}-${STATIC_CACHE_VERSION}`;
 
 // All the files need to be added here manually. I want to avoid
@@ -9,6 +9,7 @@ const STATIC_ASSETS = [
   "index.html",
   "favicon.ico",
   "manifest.json",
+  "README.md",
 
   /* JS */
   "index.mjs",

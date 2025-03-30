0bc8434bc699681036ac82ca4fa50c075ec84786
Ondřej Synáček <ondrej@synacek.org>
2025-03-30 13:19:30 +0200

    make center of cursor transparent

--- js/canvas-utils.mjs
+++ js/canvas-utils.mjs
@@ -2,38 +2,42 @@ import { COLOR } from "./state/constants.mjs";
 import { getCursorCanvas } from "./dom.mjs";
 
 const ctx = getCursorCanvas().getContext("2d");
-const CURSOR_SIZE = 20;
-
-function offsetCursorX(x) {
-  return x - CURSOR_SIZE / 2;
-}
+const CURSOR_SIZE = 10;
+const CURSOR_PART_SIZE = CURSOR_SIZE / 2;
+const GAP = CURSOR_SIZE / 2;
+const LINE_WIDTH = 5;
 
 export function drawCursor(x, y) {
-  x = offsetCursorX(x);
   ctx.clearRect(0, 0, ctx.canvas.width, ctx.canvas.height);
-  const half = CURSOR_SIZE / 2;
 
-  ctx.lineWidth = 5;
+  ctx.lineWidth = LINE_WIDTH;
   ctx.lineCap = "round";
   ctx.strokeStyle = COLOR.BLACK;
 
+  /* --- Cursor --- */
+  // Draw top line
   ctx.beginPath();
-  ctx.moveTo(x, y);
-  ctx.lineTo(x + CURSOR_SIZE, y);
+  ctx.moveTo(x, y - GAP);
+  ctx.lineTo(x, y - CURSOR_PART_SIZE - GAP - CURSOR_SIZE);
   ctx.stroke();
 
+  // Draw bottom line
   ctx.beginPath();
-  ctx.moveTo(x + half, y - half);
-  ctx.lineTo(x + half, y + half);
+  ctx.moveTo(x, y + GAP);
+  ctx.lineTo(x, y + CURSOR_PART_SIZE + GAP + CURSOR_SIZE);
+  ctx.stroke();
+
+  // Draw left line
+  ctx.beginPath();
+  ctx.moveTo(x - GAP, y);
+  ctx.lineTo(x - CURSOR_PART_SIZE - GAP - CURSOR_SIZE, y);
+  ctx.stroke();
+
+  // Draw right line
+  ctx.beginPath();
+  ctx.moveTo(x + GAP, y);
+  ctx.lineTo(x + CURSOR_PART_SIZE + GAP + CURSOR_SIZE, y);
   ctx.stroke();
 
   ctx.closePath();
-
-
-  ctx.moveTo(x + half, y + half);
-  ctx.strokeStyle = COLOR.WHITE;
-  ctx.fillStyle = COLOR.WHITE;
-  ctx.beginPath();
-  ctx.arc(x + half, y, 3, 0, Math.PI * 2);
-  ctx.fill();
 }
